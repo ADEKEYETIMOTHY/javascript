0da14a45f67e63c9c041cd2815c2a2a58a2f1a07
ADEKEYE TIMOTHY <adekeyetimothy290@gmail.com>
2024-12-05 11:57:20 +0100

    done

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
-/console.log("hello")/
-//let name = "timothy";//
-//const name2 = "adekeye";
-//let num = 2;
-//let sch = "yaba";
-//console.log(name);
-//console.log(name2);
-//console.log(num);
-//console.log(sch);
-//let user = "ELIZABETH";
-//console.log(`${user} this is my wife name`); 
-//let digit1 = 2;
-//let digit2 = 5;
-//console.log(digit1+digit2);
-//let a = "emm"
-//let b = "sam";
-//console.log(a+b);
-let surname = "ADEKEYE";
-console.log(`this is my surame ${surname}`);
-let a = 10;
-let b = 30;
-console.log (a+b);
-console.log (b&&a);
-console.log (a*b);
-console.log (a/b);
 
+
+//let a = 5;
+//let b= 10;
+//let sum = a < b // a>b
+
+let c = 5;
+let d = 10;
+let add = c + d ;
+let sub = d-c;
+let sum = c < d;
+let product = c > d;
+let diff = c < d && c>d;
+let tim = c<d || c>d;
+console.log (add);
+console.log(sub);
+console.log (sum);
+console.log (product);
+console.log (diff);
+console.log(tim);
 
 
