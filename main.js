802ddecccd3a6997ac9cfa9f19d13ef641b7d212
ADEKEYE TIMOTHY <adekeyetimothy290@gmail.com>
2024-12-09 11:30:26 +0100

    monday

--- main.js
+++ main.js
@@ -1,22 +1,68 @@
+//Control flow
+//let name = "TIMOTHY";
+//if (name === "TIMOTHY") {
+  //  console.log(name);
+    //}else{
+    //    console.log("this not your name");
+   // }
+//let name = "TIMOTHY";
+//if (name === "TIMOTHY") {
+  //  console.log("hi TIMOTHY");
+    //}else if (name === "tunde") {
+      //  console.log("WELCOME TUNDE");
+    //}else{
+      //  console.log("this is not your name")
+    //}
 
+ // let country = "NIGERIA";
+//if (country === "NIGERIA"  || country === "AMERICA");{
+  //  console.log ("i love NIGERIA");
+//}
+//let firstName ="TIMOTHY";
+//let lastName = "ADEKEYE";
+//if (firstName === "TIMOTHY"  &&  lastName === "ADEkEYE");
+//{
+  //  console.log(firstName && lastName);
+//}
 
-//let a = 5;
-//let b= 10;
-//let sum = a < b // a>b
+//let count = 5;
+//console.log (++count);
+//let a = 4;
+//let b = 8;
+//let c = 12;
+//console.log(a+b+c);
+//console.log(a > b && b > c);
+//console.log (a< b || b >c);
+//let name = " TIMOTHY";
+//console.log (name);
 
-let c = 5;
-let d = 10;
-let add = c + d ;
-let sub = d-c;
-let sum = c < d;
-let product = c > d;
-let diff = c < d && c>d;
-let tim = c<d || c>d;
-console.log (add);
-console.log(sub);
-console.log (sum);
-console.log (product);
-console.log (diff);
-console.log(tim);
+//CONTROL FLOW STATEMENT
 
-
+//let myname = (abc);
+//if (condition){
+  //retrun value or output
+//}
+//let myName = "ADEKEYE";
+//if(myName== "OLA"){
+  //console.log("that is name");
+//}else{
+  //console.log("error");
+//}
+//let a =10;
+//if (a>20){
+  //console.log("above")
+//}else if (a===20){
+  //console.log("a is equal to 20")
+//}else{
+  //console.log("a is bellow 20");
+//}
+let score =70;
+if (score==70){
+  console.log("grade  A");
+}else if (score >= 60){
+  console.log("this is B")
+}else if (score <=50){
+  console.log("grade C ");
+}else{
+  console.log("grade is F9");
+}
