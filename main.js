13a4d3436266d081be5e4a5c3d2a7db653ceb0c5
ADEKEYETIMOTHY <146393500+ADEKEYETIMOTHY@users.noreply.github.com>
2024-12-12 23:45:41 +0100

    switch

--- main.js
+++ main.js
@@ -56,13 +56,47 @@
 //}else{
   //console.log("a is bellow 20");
 //}
-let score =70;
-if (score==70){
-  console.log("grade  A");
-}else if (score >= 60){
-  console.log("this is B")
-}else if (score <=50){
-  console.log("grade C ");
-}else{
-  console.log("grade is F9");
-}
+//let score =70;
+//if (score==70){
+  //console.log("grade  A");
+//}else if (score >= 60){
+  //console.log("this is B")
+//}else if (score <=50){
+  //console.log("grade C ");
+//}else{
+  //console.log("grade is F9");
+//}
+
+//let age = 23
+//if (age = 18){
+  //console.log("powering on . Enjoy the ride")
+//}else if (age = 18){
+  //console.log("congratulation on your first year of drive")
+//}else{
+  //console.log("you are too young to drive")
+//};
+// switch statement alarm
+// monday -5:00 am
+//tuesday - 6:00 am
+//wednesday - 9:00am
+// thursday - 10:00am
+// friday - 8:00 am
+let day = "sunday";
+switch(day){
+  case "monday" :
+    console.log("7am");
+    break
+    case "tuesday":
+      console.log("5am");
+      case "wednesday":
+        console.log("6am");
+        case "thursday":
+          console.log("10 am")
+          case "friday" :
+            console.log("9am")
+            case "saturday" :
+              console.log("11am")
+              case "sunday":
+                console.log("4am")
+          };
+          console.log("Thank you")
