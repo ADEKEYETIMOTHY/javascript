12b1837bae5fb2d59c8d19968fce1aeaed1ec6c8
ADEKEYETIMOTHY <146393500+ADEKEYETIMOTHY@users.noreply.github.com>
2025-01-22 11:45:43 +0100

    today class

--- new.js
+++ new.js
@@ -36,5 +36,84 @@
   //}
   //console.log(findLargestNum(1,2,3));
 
-  let user = "Timothy";
-  console.log("my is user");
+  //let userName = "Timothy";
+  //console.log(`this is ${userName}`);
+
+
+  //let a = 10;
+  //let b = 15;
+  //let condition = a>b && a<b
+  //console.log(condition);
+
+  // GLOBAL SCOPE
+  //BLOCK SCOPE
+  
+// CALLBACK FUNCTION
+
+//let digit = 2
+//{ 
+   // let number = digit
+  //  console.log(number);
+    
+//}
+
+
+//let a = 30
+//let b = 50
+//let results = console.log(a + b);
+//function sum() {
+ //results;   
+//}
+
+//sum();
+
+
+//function display (result) {
+    //console.log(resuit)
+  //  }
+
+//function add (num1 , num2, myCallback) {
+   //let sum = num1 + num2
+  //  myCallback(sum)
+//}
+//add(20, 30, display)
+
+
+// OBJECT
+//const detailOfperson = {
+//name:"OLANIKE",
+//surName:"Adekeye",
+//age:25,
+//adress: "lagos",
+//sex:"f",
+//mar:"y",
+//edu:"bachor",
+
+
+
+
+//}
+//console.log(detailOfperson.surName);
+
+
+
+//let information = {
+
+    //name:"TIMOTHY",
+    //surName:"adekeye",
+    //age:32,
+    //address:"agege",
+   //sta :"lagos",
+  // mar:"y"
+   
+//}
+//console.log(information.name);
+
+
+
+function root(digit){
+    console.log(digit **3);
+    
+
+}
+    root(4)
