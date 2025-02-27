6c953cbf1ce454e458365d3a8e34380f4ffccca9
ADEKEYETIMOTHY <146393500+ADEKEYETIMOTHY@users.noreply.github.com>
2025-02-27 10:28:16 +0100

    project

--- new.js
+++ new.js
@@ -1,119 +1,28 @@
-//function findLargestNum(num1, num2, num3) {
-  //  let max = num1;
-//}
-//if (num2 > max) {
-  //  console.log("max is num2")
-    
-//}
-//else if (num3 >max) {
-  //  console.log("max is num3")
-    
-//} else {
-  //  console.log("num1 is max")
-    
-//}
+//let h1 = document.getElementById("main");
+//h1.style.color = "green";
 
-//findLargestNum(5, 10, 15)
+
+ //Query
+ //let firsth1 = document.querySelector("#a");
+ //firsth1.style.color = "red";
+ //firsth1.innerHTML = "love";
 
 
 
+//let secondh1 = document.querySelector("#b");
+ //secondh1.style.backgroundColor = "black"
+ //secondh1.style.color = "yellow";
+ //secondh1.textContent = "user";
+ //console.log(document.body.firstChild);
+
+ //let lasth1 = document.querySelector("#c");
+ //lasth1.style.backgroundColor = "black";
+ 
+ //textcon
 
 
-//function findLargestNum(num1, num2, num3) {
-
-    //if (num1 >= num2 && num1 >= num3) {
-      //return num1
-      
-    //}
-  
-  //else if (num2 >= num1 && num2 >= num3) {
-    //return num2
-    
-  //} else {
-    //return num3 
-    
-  //}
-  //}
-  //console.log(findLargestNum(1,2,3));
-
-  //let userName = "Timothy";
-  //console.log(`this is ${userName}`);
+ let h1 = document.getElementById("hi");
+ 
 
 
-  //let a = 10;
-  //let b = 15;
-  //let condition = a>b && a<b
-  //console.log(condition);
-
-  // GLOBAL SCOPE
-  //BLOCK SCOPE
-  
-// CALLBACK FUNCTION
-
-//let digit = 2
-//{ 
-   // let number = digit
-  //  console.log(number);
-    
-//}
-
-
-//let a = 30
-//let b = 50
-//let results = console.log(a + b);
-//function sum() {
- //results;   
-//}
-
-//sum();
-
-
-//function display (result) {
-    //console.log(resuit)
-  //  }
-
-//function add (num1 , num2, myCallback) {
-   //let sum = num1 + num2
-  //  myCallback(sum)
-//}
-//add(20, 30, display)
-
-
-// OBJECT
-//const detailOfperson = {
-//name:"OLANIKE",
-//surName:"Adekeye",
-//age:25,
-//adress: "lagos",
-//sex:"f",
-//mar:"y",
-//edu:"bachor",
-
-
-
-
-//}
-//console.log(detailOfperson.surName);
-
-
-
-//let information = {
-
-    //name:"TIMOTHY",
-    //surName:"adekeye",
-    //age:32,
-    //address:"agege",
-   //sta :"lagos",
-  // mar:"y"
-   
-//}
-//console.log(information.name);
-
-
-
-function root(digit){
-    console.log(digit **3);
-    
-
-}
-    root(4)
+ 
